7527feb53c6bd1d1dc404d522a952dc323b605de
cooldude72463 <kieransmollet0@gmail.com>
2018-04-22 22:38:25 +0100

    yes

--- JS/TMDBAPI.js
+++ JS/TMDBAPI.js
@@ -123,7 +123,7 @@ function addResultTitles(jsondata){
       var img = "<img src="+poster+" alt=Poster for "+title+">";
 
       var link = "MoviePage?q=" + url;
-      var temp1 = "<div class = movieLink>"
+      var temp1 = "<div class = flex-item>"
       var temp2 = "<a href="+ link +">"
       var temp3 = "<div class = poster>" + img + "</div>"
       var temp4 = "<div class = title>" + title + "</div>"
