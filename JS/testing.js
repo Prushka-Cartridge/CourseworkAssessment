ff07b5ba356de8aa988b88c4f60df6d13ab01a81
cooldude72463 <kieransmollet0@gmail.com>
2018-04-24 10:29:57 +0100

    h

--- JS/testing.js
+++ JS/testing.js
@@ -1,4 +1,11 @@
+var db;
 function main(){
+  MongoClient.connect(url, function(err, database) {
+    if (err) throw err;
+    db = database;
+    console.log('listening on 8080');
+  });
+  
   var btn = document.querySelector('input');
   btn.addEventListener('click', deleteDatabaseItems);
   console.log("no")
