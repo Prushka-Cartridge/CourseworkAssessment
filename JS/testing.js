39639bee3aba9ba2b16686bc720fbb59fc02b5c7
cooldude72463 <kieransmollet0@gmail.com>
2018-04-24 10:23:44 +0100

    h

--- JS/testing.js
+++ JS/testing.js
@@ -1,7 +1,9 @@
 var btn = document.querySelector('input');
 btn.addEventListener('click', deleteDatabseItems);
+console.log("no")
 
 function deleteDatabseItems(){
+  console.log("yes")
   db.collection('UserInfo').drop(function(err, result){
     if (err) throw err;
     console.log('Login')
