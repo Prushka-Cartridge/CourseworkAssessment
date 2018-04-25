1341a9919f85bcccd31be9a7c7f95f7fa6a12e6d
cooldude72463 <kieransmollet0@gmail.com>
2018-04-25 05:08:21 +0100

    h

--- server.js
+++ server.js
@@ -38,11 +38,11 @@ app.get('/testing', function(req, res) {
 
 
 app.post('/Login', function(req, res) {
-  console.log(window.document.URL);
+  console.log(global.document.URL);
     db.collection('UserInfo').find().toArray(function(err, result){
       if (err) throw err;
       console.log('Login')
-      res.redirect(window.document.URL);
+      res.redirect(global.document.URL);
       })
 })
 
@@ -50,7 +50,7 @@ app.post('/SignUp', function(req, res) {
   db.collection('UserInfo').save(req.body, function(err, result) {
     if (err) throw err;
     console.log('SignUp')
-    res.redirect(window.document.URL)
+    res.redirect(global.document.URL)
   })
 })
 
@@ -59,7 +59,7 @@ app.post('/testing', function(req, res) {
   db.collection('UserInfo').drop(function(err, result){
     if (err) throw err;
     console.log('test')
-    res.redirect(window.document.URL)
+    res.redirect(global.document.URL)
   })
 })
 
