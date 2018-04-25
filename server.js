249664404e9af0f4c8cfd48ef7c351ca616828a3
cooldude72463 <kieransmollet0@gmail.com>
2018-04-25 23:54:30 +0100

    h

--- server.js
+++ server.js
@@ -81,11 +81,13 @@ app.post('/Login', function(req, res) {
             console.log("Username is in the system");
             return;
         } else {
+            alert("Incorrect Password")
             console.log("Username isn't in the system");
             res.redirect("/");
             return;
         }
     })
+    alert("That user does not exist")
 })
 
 app.post('/LogOut', function(req, res) {
