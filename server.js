b1be0c3b3a814523d2a4104d47e5892063b2cb68
cooldude72463 <kieransmollet0@gmail.com>
2018-04-25 05:15:00 +0100

    h

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get('/testing', function(req, res) {
 
 
 app.post('/Login', function(req, res) {
-  console.log(global.document.location);
+  console.log(location.href);
     db.collection('UserInfo').find().toArray(function(err, result){
       if (err) throw err;
       console.log('Login')
