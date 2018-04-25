39e6083413aa37462b9cc58a7ed9f6a87f7ee12b
cooldude72463 <kieransmollet0@gmail.com>
2018-04-25 22:46:44 +0100

    h

--- server.js
+++ server.js
@@ -66,9 +66,14 @@ app.get('/testing', function(req, res) {
 app.post('/Login', function(req, res) {
     db.collection('UserInfo').find().toArray(function(err, result){
         if (err) throw err;
-        console.log('Login')
-        req.session.loggedin = true;
-        res.redirect("/");
+        for(int i = 0; i < result.length; i++){
+            if(result.username == req.body.username && result.password == req.body.password){
+                console.log('Login')
+                req.session.loggedin = true;
+                res.redirect("/");
+            }
+        }
+        console.log("Username isn't in the system");
     })
 })
 
