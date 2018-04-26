142c1460bce3873b06a25742a993b7401cecebf1
cooldude72463 <kieransmollet0@gmail.com>
2018-04-26 21:27:38 +0100

    s

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.get('/MoviePage', function(req, res) {
     //console.log("Movie Title"+movieTitle)
 
     db.collection('MovieInfo').find({"MovieInfo.title":movieTitle}).toArray(function(err, results) {
-        var array[results.length];
+        var array = [results.length];
         if (err) throw err;
         console.log(results)
         if(results.length == 0){
