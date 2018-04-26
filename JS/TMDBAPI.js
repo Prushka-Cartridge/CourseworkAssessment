704e3e3ffb5907ed4a303717ecdbb2f5ff81bd73
cooldude72463 <kieransmollet0@gmail.com>
2018-04-26 19:53:25 +0100

    testing

--- JS/TMDBAPI.js
+++ JS/TMDBAPI.js
@@ -129,7 +129,7 @@ function addResultTitles(jsondata){
       var img = "<img src="+poster+" alt=Poster>";
 
       var link = "MoviePage?q=" + url;
-      var temp1 = "<form><button type = submit> <div class = flex-item>"
+      var temp1 = "<div class = flex-item> <form><button type = submit action=/"+url+" method=GET>"
       var temp2 = "<a href="+ link +">"
       var temp3 = "<div class = poster>" + img + "</div>"
       var temp4 = "<div class = title>" + title + "</div>"
