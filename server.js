88dd21eadf922383508a3086fc953e9cf1e02521
cooldude72463 <kieransmollet0@gmail.com>
2018-04-26 22:03:25 +0100

    s

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ app.get('/MoviePage', function(req, res) {
         }
         //console.log("Does this actually work "+output);
     })
-    console.log(array)
+    console.log("n"+array)
     if(req.session.loggedin){
         db.collection('UserInfo').findOne({"login.username":username}, function(err, result) {
         res.render('pages/MoviePageLoggedIn', {user: result, array: array});
