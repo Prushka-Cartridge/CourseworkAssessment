88a2ebe7acefe2ba3818fc9d3ddfc9d574289c1d
cooldude72463 <kieransmollet0@gmail.com>
2018-04-26 21:39:56 +0100

    s

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-const ejsLint = require('ejs-lint');
+
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017/MovieReviews";
 const session = require('express-session'); //npm install express-session
@@ -98,7 +98,6 @@ app.get('/MoviePage', function(req, res) {
             }
         }
         let ejs = require('ejs');
-        console.log(ejslint('<%= array.join(); %>', {array: array}))
         html = ejs.render('<%= array.join(); %>', {array: array});
         //console.log("Does this actually work "+output);
     })
