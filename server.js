6f646a2ac0444e01cc389541af2ee642ae5e79d7
cooldude72463 <kieransmollet0@gmail.com>
2018-04-26 21:10:10 +0100

    yeah

--- server.js
+++ server.js
@@ -86,17 +86,13 @@ app.get('/MoviePage', function(req, res) {
     db.collection('MovieInfo').find({"MovieInfo.title":movieTitle}).toArray(function(err, results) {
         if (err) throw err;
         //console.log(results)
-        console.log(movieTitle);
-        console.log(results.length)
         if(!results){
             output += "No reviews exist for this movie";
         } else {
             for(var i = 0; i < results.length; i++){
                 console.log("Boi")
-                //output += "<div> <div>Created By:"+results[i].login.username+"</div>";
-                //output += "<div> Review:"+results[i].MovieReview.review+"</div> </div>"
-                output += "Created By:"+results[i].login.username+"";
-                output += "Review:"+results[i].MovieReview.review+""
+                output += "<div> <div>Created By:"+results[i].login.username+"</div>";
+                output += "<div> Review:"+results[i].MovieReview.review+"</div> </div>"
             }
         }
         console.log("Does this actually work "+output);
